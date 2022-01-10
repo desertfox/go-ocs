d46ff4b39dd9e834777493a1cb5575c7061b3908
Rummy <cerealbh@gmail.com>
2022-01-10 13:56:20 -0600

    Patched OKD3 bug
    OKD3 doesn't user --server flag and breaks flat parsing, added one off check to fix behavior.

--- main.go
+++ main.go
@@ -30,27 +30,26 @@ func init() {
 		selfupdate.EnableLog()
 	}
 
-	if len(os.Args) > 1 {
-		CLICommand = os.Args[1]
-	} else {
-		CLICommand = ""
-	}
-
 	FlagSet = flag.FlagSet{}
-
 	FlagSet.StringVar(&CLIServer, "server", "", "Server")
 	FlagSet.StringVar(&CLIToken, "token", "", "Token")
-
 }
 
 func main() {
 
 	if len(os.Args) > 1 {
+		CLICommand = os.Args[1]
+
 		FlagSet.Parse(os.Args[2:])
 
 		if os.Args[1] == "help" || os.Args[1] == "h" {
 			fmt.Println(readme)
 			os.Exit(0)
+		}
+
+		if os.Args[1] == "login" {
+			CLIServer = os.Args[2]
+			FlagSet.Parse(os.Args[3:])
 		}
 	}
 
